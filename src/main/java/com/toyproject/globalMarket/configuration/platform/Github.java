10b7abe6577de214d4be1941c9e5e0ef313539ae
hyeonwoody <hyeonwoody@gmail.com>
2024-02-17 13:04:46 +0900

    🔊 LOGUP : Add logging on Github class

--- src/main/java/com/toyproject/globalMarket/configuration/platform/Github.java
+++ src/main/java/com/toyproject/globalMarket/configuration/platform/Github.java
@@ -2,19 +2,24 @@ package com.toyproject.globalMarket.configuration.platform;
 
 import com.toyproject.globalMarket.DTO.product.platform.naver.Images;
 import com.toyproject.globalMarket.VO.product.ProductRegisterVO;
+import com.toyproject.globalMarket.libs.BaseObject;
 
 import java.io.IOException;
 import java.util.Arrays;
 
-public class Github {
+public class Github extends BaseObject {
+
+    private static int objectId = 0;
     public final String uploadDirectory = System.getProperty("user.dir") + "/src/main/resources/";
     public final String uploadThumbnailDirectory = System.getProperty("user.dir") + "/src/main/resources/detail/thumbnail";
+
 
     private String id;
     private String nickname;
     private String id_nickname;
     private String[] params;
     public Github(ProductRegisterVO productSource){
+        super("Github", objectId++);
         this.id = "unique"; //Product repository to get Id
         this.nickname = "branch";
         this.params = new String[]{id + "_" + nickname, productSource.getName()};
@@ -23,6 +28,18 @@ public class Github {
         final String init = "init.sh";
         ProcessBuilder processBuilder = new ProcessBuilder();
         processBuilder.command(Arrays.asList("sh", uploadDirectory+init, this.params[0], this.params[1]));
+        Process process = null;
+        try {
+            process = processBuilder.start();
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+                LogOutput(LOG_LEVEL.INFO, ObjectName(), MethodName(), 0, "Init script executed successfully.");
+            } else {
+                LogOutput(LOG_LEVEL.ERROR, ObjectName(), MethodName(), 2, "Init script execution failed with exit code: {0}",exitCode);
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void uploadImages() {
@@ -38,9 +55,9 @@ public class Github {
             process = processBuilder.start();
             int exitCode = process.waitFor();
             if (exitCode == 0) {
-                System.out.println("Script executed successfully.");
+                LogOutput(LOG_LEVEL.INFO, ObjectName(), MethodName(), 0, "Commit script executed successfully.");
             } else {
-                System.err.println("Script execution failed with exit code: " + exitCode);
+                LogOutput(LOG_LEVEL.ERROR, ObjectName(), MethodName(), 2, "Commit script execution failed with exit code: {0}",exitCode);
             }
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
