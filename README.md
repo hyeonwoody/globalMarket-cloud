55a91d8262c18bf8198a08ad0a3a9680b3449d0b
hyeonwoody <hyeonwoody@gmail.com>
2024-02-22 02:22:12 +0900

    🛢️DB : Add product_number for ProductEntity

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ Gson
 
 NaverAPI
 Google Drive API
+
+TODO:
+product register  error handling (invalid input)
+Replacing detailcontent image to github
+
+상세페이지 SEO html builder
--- src/main/java/com/toyproject/globalMarket/entity/ProductEntity.java
+++ src/main/java/com/toyproject/globalMarket/entity/ProductEntity.java
@@ -14,14 +14,16 @@ public class ProductEntity {
     private String platform_type;
 
     private String outsourced_url;
+    private Long product_number;
     private int sale_price;
     private int sell_count;
 
-    public void setEntity(Product _product, String _outSourced_url) {
-        this.platform_type = _product.getPlatformType().toString();
-        this.outsourced_url = _outSourced_url;
-        this.sale_price = _product.getSalePrice();
+    public void setEntity(Product product, String outSourcedUrl, Long productNumber) {
+        this.platform_type = product.getPlatformType().toString();
+        this.outsourced_url = outSourcedUrl;
+        this.sale_price = product.getSalePrice();
         this.sell_count = 0;
+        this.product_number = productNumber;
     }
 
 
