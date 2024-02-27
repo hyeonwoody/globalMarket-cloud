7066ee91c6487f98eee2039c7a580dae93096731
hyeonwoody <hyeonwoody@gmail.com>
2024-02-28 00:54:43 +0900

    📺DESIGN : Implement centered auth login frontend

--- src/main/frontend/src/pages/root/part/Auth.tsx
+++ src/main/frontend/src/pages/root/part/Auth.tsx
@@ -29,14 +29,25 @@ const Auth = (props: AuthProps) => {
         setFormData({ ...formData, password: value });
     }
 
+    const handleMouseOver = () => {
+
+    }
+
     return (
-        <div>
-                <div>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <div id={"Auth-login"} className={"mt-20"}>
                     <input
                         type="text"
                         placeholder="Username"
                         value={formData.username}
+                        className={"w-full"}
                         onChange={handleUsernameChange}
+                        style={{
+                            padding: '10px',
+                            border: '1px solid #ddd',
+                            borderRadius: '3px',
+                            fontSize: '16px',
+                        }}
                     />
                     <br/>
                     <input
@@ -45,9 +56,21 @@ const Auth = (props: AuthProps) => {
                         placeholder="Password"
                         value={formData.password}
                         onChange={handlePasswordChange}
+                        style={{
+                            padding: '10px',
+                            border: '1px solid #ddd',
+                            borderRadius: '3px',
+                            fontSize: '16px',
+                        }}
                     />
                     <br/>
-                    <button onClick={handleLogin}>Login</button>
+                    <button onClick={handleLogin} style={{
+                        color: 'white',
+                        borderRadius: '7px',
+                        cursor: 'pointer',
+                        transition: '0.3s ease all',
+
+                    }} className={"mt-2 py-2 px-2 bg-green-500 hover:bg-amber-300 w-full rounded-full"}>Login</button>
                 </div>
         </div>
     );
