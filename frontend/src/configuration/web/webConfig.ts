274b2f60fd29c8c47051a1232e42738f7d93780e
hyeonwoody <hyeonwoody@gmail.com>
2024-05-01 14:44:22 +0900

    👷BUILD : Add secretized properties

--- frontend/src/configuration/web/webConfig.ts
+++ frontend/src/configuration/web/webConfig.ts
@@ -2,8 +2,8 @@ export class My {
     ipAddress : string;
     backEndPort : string;
     constructor() {
-        this.ipAddress = "175.193.141.185";
-        this.backEndPort = "11000";
+        this.ipAddress = "{MY_IP_ADDRESS}";
+        this.backEndPort = "{MY_BACKEND_PORT}";
     }
 }
 
